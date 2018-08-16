4c60fc0a32404e801575a8f53fdd44dd4507bb92
Pavan  Kumar <pavan.thikkavarapu@object-frontier.com>
2018-08-16 17:55:05 +0530

    search filter added successfully

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  name = 'Angular';
+  characters = [
+      'Finn the human',
+      'Jake the dog',
+      'Princess bubblegum',
+      'Lumpy Space Princess',
+      'Beemo1',
+      'Beemo2'
+    ]
+}
